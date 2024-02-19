38e00274f7c25bbc3f26dcbc404703f0a71b61fa
itobuz <itobuz@itobuzs-MacBook-Air.local>
2024-02-19 15:41:37 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+function aqua1() {
+  document.body.style.backgroundColor = "aqua";
+}
+function pink1() {
+  document.body.style.backgroundColor = "pink";
+}
+function green1() {
+  document.body.style.backgroundColor = "lightgreen";
+}
+function blue1() {
+  document.body.style.backgroundColor = "lightblue";
+}
